9366170708c51eecfaee0544412e8920edc3b188
arturbednarz <slliver@icloud.com>
2024-05-03 16:34:31 +0100

    Create ball

--- ball.py
+++ ball.py
@@ -1,7 +1,8 @@
 import math
 from turtle import Turtle
 
-BALL_SPEED = 10
+# Don't set less than 5
+BALL_SPEED = 5
 
 
 class Ball(Turtle):
@@ -9,19 +10,15 @@ class Ball(Turtle):
         super().__init__("square")
         self.color("white")
         self.penup()
-        self.horizon_limit = screen_width / 2
-        self.vertical_limit = screen_height / 2 - 30
-        self.up_down = -3
-        self.left_right = -3
+        self.horizon_limit = screen_width / 2 - 20
+        self.vertical_limit = screen_height / 2 - 20
+        self.up_down = -BALL_SPEED
+        self.left_right = -BALL_SPEED
 
     def move_ball(self):
         new_xcor = self.xcor() + self.left_right
         new_ycor = self.ycor() + self.up_down
         self.goto(new_xcor, new_ycor)
-        # self.forward(self.left_right * BALL_SPEED)
-        # self.left(self.up_down * 90)
-        # self.forward(BALL_SPEED)
-        # self.right(self.up_down * 90)
 
     def bounce_left_right(self):
         self.left_right *= -1
@@ -31,3 +28,5 @@ class Ball(Turtle):
 
     def reset_ball(self):
         self.goto(0, 0)
+        self.up_down = -BALL_SPEED
+        self.left_right = -BALL_SPEED
